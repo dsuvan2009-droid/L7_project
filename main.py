72add2003948392b540e1b1bf5d6fe9afbe65c92
dsuvan2009-droid <dsuvan2009@gmail.com>
2026-03-13 20:52:38 +0800

    secondcommit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 age=int(input("Enter your age: "))
-if age>=10 and age<=18:
+if age>10 and age<=18:
     print("You are a teenager.")
 elif age>18:
     print("You are an adult.")
 else:
-    print("Invalid age.")
+    if  age<=10 and age>=0:
+        print("You are a child.")
+    else:
+     print("Invalid age.")
